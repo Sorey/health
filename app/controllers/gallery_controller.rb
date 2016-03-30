3a6efe1296ffd3740fc76a464bd4d3e43a6f86c7
Sorey <3sorey4@gmail.com>
2016-03-30 12:56:59 +0300

    run assets, fancybox to assets not multi upload

--- app/controllers/gallery_controller.rb
+++ app/controllers/gallery_controller.rb
@@ -1,22 +1,10 @@
 class GalleryController < ApplicationController
-  def galleries
 
+  def galleries
   end
 
   def show
       @name_gallery = params[:gallery] ||= "Картограмми"
       @images = Dir.glob("app/assets/images/statistic_gallery/#{@name_gallery}/*.jpg")
-      # respond_to do |format|
-      #   format.html { render :show }
-      # end
-      # format.html { redirect_to gallery_url(@images) }
-      # if @admin_statistic_catalog.update(statistic_catalog_params)
-      #
-      #   format.html { redirect_to @admin_statistic_catalog, notice: 'Statistic catalog was successfully updated.' }
-      #   format.json { render :show, status: :ok, location: @admin_statistic_catalog }
-      # else
-      #   format.html { render :edit }
-      #   format.json { render json: @admin_statistic_catalog.errors, status: :unprocessable_entity }
-      # end
   end
 end

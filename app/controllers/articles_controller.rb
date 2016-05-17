cb528c85631df166ab2202d60f039562852bcbc7
Sorey <3sorey4@gmail.com>
2016-05-17 17:54:53 +0300

    change todo, fix not show false regional_programs

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
 
   def show_articles_group
     @articles_group = Admin::ArticlesGroup.find_by(title_english: params[:title_group])
-    @articles = Admin::Article.where(:admin_articles_group_id => @articles_group.id)
+    @articles = Admin::Article.where(publish_on: true, :admin_articles_group_id => @articles_group.id)
   end
 
   def show_article

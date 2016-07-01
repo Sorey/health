1e409dcb72a34e6a2849c2958095080fbfe5f0d2
Sorey <3sorey4@gmail.com>
2016-07-01 14:04:54 +0300

    set title page where necessary

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,16 +2,17 @@ class ArticlesController < ApplicationController
 
   def show_articles_group
     @articles_group = Admin::ArticlesGroup.find_by(title_english: params[:title_group])
+    set_title_page @articles_group.title
     @articles = Admin::Article.where(publish_on: true, :admin_articles_group_id => @articles_group.id).page(params[:page]).per(30)
   end
 
   def show_article
     @article = Admin::Article.find(params[:id])
-
     meta_data @article
   end
 
   def show_groups_article
     @article = Admin::Article.find_by(title_english: params[:title_article])
+    meta_data @article
   end
 end

4883eae1661c787c29641f14cf5e0764602dd9f9
Sorey <3sorey4@gmail.com>
2016-07-05 16:17:40 +0300

    improve logic menu_items

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,8 +23,7 @@ class ApplicationController < ActionController::Base
       if menu_item.parent_id.to_s.to_i == 0
         @menu << "<li class='dropdown'><a class='dropdown-toggle' aria-expanded='false' aria-haspopup = 'true' data-toggle = 'dropdown' href = '#{menu_item.link}' role = 'button' target= 'blank'> #{menu_item.title} <span class='caret'></span></a>" if menu_item.type_level == 'Заголовок меню'
         @menu << "<li > <a href = '#{menu_item.link}' target='#{menu_item.target}'> #{menu_item.title} </span></a>" if menu_item.type_level == 'Пункт меню'
-        # get_children menu_item.id, menu_item.type_level if menu_item.children.any?
-        get_children menu_item, menu_item.type_level if menu_item.children.any?
+        get_children menu_item.id, menu_item.type_level if menu_item.children.any?
         @menu << '</li>'
       end
     end
@@ -42,29 +41,8 @@ class ApplicationController < ActionController::Base
       @meta_keywords = page.meta_keywords unless page.meta_keywords.blank?
       @meta_description = page.meta_description unless page.meta_keywords.blank?
     end
-    # .sort_by(&:order_answer)
-    def get_children r_menu_item, t_level
-      @menu << '<ul class="dropdown-menu">' if t_level == 'Заголовок меню'
-      @menu << '<ul class="deep-level">' if t_level != 'Заголовок меню'
-      iterator_separator = 0
-      r_menu_item.children.where(show: true).sort_by(&:order_item).each do |menu_item|
-        @menu << '<li class="divider", role = "separator"></li>' if iterator_separator > 0 && t_level == 'Заголовок меню'
-        unless menu_item.alias.blank? || params[:controller] == 'ckeditor/pictures' # != 'home' Need for upload image with CkEditor and not show this menu_items
-          m_i_alias = url_for(['menu_item', menu_item.alias])
-          @menu << "<li><a href='#{m_i_alias}'> #{menu_item.title}</a>" if t_level == 'Заголовок меню'
-          @menu << "<li><a href='#{m_i_alias}'> #{menu_item.title}</a>" if t_level != 'Заголовок меню'
-        else
-          @menu << "<li><a href='#{menu_item.link}' target='#{menu_item.target}'> #{menu_item.title}</a>" if t_level == 'Заголовок меню'
-          @menu << "<li><a href='#{menu_item.link}' target='#{menu_item.target}'> #{menu_item.title}</a>" if t_level != 'Заголовок меню'
-        end
-        iterator_separator += 1
-        get_children menu_item, menu_item.type_level if menu_item.children.any?
-        @menu << '</li>'
-      end
-      @menu << '</ul>'
-    end
 
-    def get_children1 id, t_level
+    def get_children id, t_level
       @menu << '<ul class="dropdown-menu">' if t_level == 'Заголовок меню'
       @menu << '<ul class="deep-level">' if t_level != 'Заголовок меню'
       iterator_separator = 0

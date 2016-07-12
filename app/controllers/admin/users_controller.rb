8cb38c70e6a88d3d2372f7a035576926fa2d38c8
Sorey <3sorey4@gmail.com>
2016-07-12 16:12:16 +0300

    simple refactor users controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,11 +1,12 @@
 module Admin
   class UsersController < AdminController
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
+
     def index
       @users = Admin::User.all
     end
 
     def show
-      @user = Admin::User.find(params[:id])
     end
 
     def new
@@ -13,12 +14,10 @@ module Admin
     end
 
     def edit
-      @user = Admin::User.find(params[:id])
     end
 
     def create
       @user = Admin::User.new(user_params)
-
       respond_to do |format|
         if @user.save
           format.html { redirect_to @user, notice: 'User was successfully created.' }
@@ -31,8 +30,6 @@ module Admin
     end
 
     def update
-      @user = Admin::User.find(params[:id])
-
       if params[:admin_user][:password].blank? && params[:admin_user][:password_confirmation].blank?
         params[:admin_user].delete(:password)
         params[:admin_user].delete(:password_confirmation)
@@ -45,22 +42,23 @@ module Admin
         else
           format.html { render action: 'edit' }
           format.json { render json: @user.errors, status: :unprocessable_entity }
-
         end
       end
     end
 
     def destroy
-      @user = Admin::User.find(params[:id])
       @user.destroy
-
       redirect_to admin_users_path
     end
 
-  private
+    private
 
-    def user_params
-      params.require(:admin_user).permit(:name, :full_name, :email, :password, :password_confirmation, admin_role_ids: [])
-    end
+      def set_user
+        @user = Admin::User.find(params[:id])
+      end
+
+      def user_params
+        params.require(:admin_user).permit(:name, :full_name, :email, :password, :password_confirmation, admin_role_ids: [])
+      end
   end
 end
